23421327e0d35e83662cdc52134dcb4540f1355a
rbrunob <bruno.bragaramos@gmail.com>
2023-02-27 15:42:44 -0300

    👨‍💻FEAT: Inclusão menu hamburguer mobile

--- src/includes/header.php
+++ src/includes/header.php
@@ -10,5 +10,21 @@
                 <a rel="noopener" target="_blank" href="https://gremioplay.com.br/sign-in">Faça Login</a>
             </div>
         </div>
+        <div class="nav_mobile">
+            <div class="menu_container">
+                <div class="menu_lines"></div>
+                <div class="menu_lines"></div>
+                <div class="menu_lines"></div>
+            </div>
+
+            <div class="menu_nav_mobile">
+                <a rel="noopener" target="_blank" href="https://cadastro.gremioplay.tv.br/">Cadastre-se Gratuitamente</a>
+                <a rel="noopener" target="_blank" href="https://gremioplay.com.br/sign-in">Faça Login</a>
+                <a rel="noopener" target="_blank" href="https://gremioplay.tv.br/radio/index.php">Ouça Rádio Grêmio</a>
+            </div>
+        </div>
     </nav>
+
+
+
 </header>
